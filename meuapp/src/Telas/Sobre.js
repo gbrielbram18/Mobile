371f98c8f126844e07950956e74451b5085e1fb4
gbrielbram18 <biel300816@gmail.com>
2025-10-17 15:13:00 -0300

    adicionando novo arquivo que puxa os dados, referente a KEY, onde vai puxar os dados no "ListaContato.js" onde vai aparecer os dados ja existentes de alguem ou algo.

--- meuapp/src/Telas/Sobre.js
+++ meuapp/src/Telas/Sobre.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
-import Card from "../Components/Card"
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import Image1 from "../../assets/img-1.jpg";
+import Image2 from "../../assets/img-2.jpg";
 
-export default function Home() {
-  const navigation = useNavigation();
+export default function Sobre() {
+  const info = {
+    nomeApp: "Fut Popeto",
+    versao: "1.0.0",
+    desenvolvedor: "Pessoa Dev"
+  };
 
   return (
-    <View>
-        <Card 
-          title="Sobre"
-          content="Saiba mais sobre nós e nossos serviços."
-          textButton="Ir para Sobre *"
-          onPress={ () => navigation.navigate('Sobre') }
-        />
-        <Card
-          title="Faq"
-          content="Saiba mais Faq"
-          textButton="Ir para Faq"
-          onPress={ () => navigation.navigate('Faq') }
-        />
-    </View>
+    <ScrollView style={estilos.container}>
+        <Text>Sobre</Text>
+        <Image source={Image1} style={estilos.image} />
+        <View style={estilos.line} />
+        <Image source={Image2} style={estilos.image} />
+        <View style={estilos.line} />
+        <Text style={estilos.titulo}>Sobre o APP</Text>
+        <Text>Nome: {info.nomeApp}</Text>
+        <Text>Versão: {info.versao}</Text>
+        <Text>Desenvolvido por: {info.desenvolvedor}</Text>
+    </ScrollView>
   )
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+  image: {
+    margin: 10,
+    borderRadius: 12,
+    borderColor: "orange",
+    borderWidth: 2
+  },
+  line: {
+    width: "90%",
+    height: 1,
+    backgroundColor: "#ccc",
+    marginVertical: 10
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10
+  }
+})
